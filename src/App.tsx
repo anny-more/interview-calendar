d64d13d61db571b0521c4c3a2072a93fb7f7f639
anny_more <anxxxen@ya.ru>
2023-04-21 11:17:40 +0300

    feat: add week to show

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {getWeek} from './modules/getWeek';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Wrapper = styled.div`
+    margin: 0 auto;
+    max-width: 720px;
+    min-width: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 2rem;
+    color: red;
+`;
+
+const Calendar = styled.div`
+    display: flex;
+    flex-direction: row:
+    align-items: stretch;
+    justify-content: space-evenly;
+    width: 100%;
+    padding: 10px;
+
+`;
+const Day = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+
+`;
+
+function App (): JSX.Element {
+    const [dateInfo, setDate] = useState(getWeek.setWeek());
+
+    return (
+        <Wrapper>
+            <Calendar>
+                <Day></Day>
+                {dateInfo.map(({item, day, date}) => {
+                    return (
+                        <>
+                            <Day key={item.toString()}>
+                                <div>{day}</div>
+                                <div>{date}</div>
+                            </Day>
+                        </>
+                    );
+                })}
+            </Calendar>
+            <Calendar>
+                <button onClick={() => {setDate(getWeek.getDayBefore());}}>Prev</button>
+                <div>{dateInfo[0].month}  {dateInfo[0].year}</div>
+                <button onClick={() => {setDate(getWeek.getDayAfter());}}>Next</button>
+            </Calendar>
+        </Wrapper>
+    );
 }
 
 export default App;
